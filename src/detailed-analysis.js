6250573136307314506f607b3b1f4089f2a88bd1
Robert Reutiman <rreutima@nd.edu>
2020-11-03 22:58:47 -0500

    Filters and Selectors implemented, row data populated

--- src/detailed-analysis.js
+++ src/detailed-analysis.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import teamStrings from './team-strings';
+import TableFilter from './TableFilter';
 
 class DetailedAnalysis extends React.Component {
 
@@ -6,7 +8,8 @@ class DetailedAnalysis extends React.Component {
     super(props);
     this.state = {
       filter: "goals",
-      entries_per_page: 20,
+      position: "ALL",
+      entries_per_page: 10,
       page: 1,
     };
   }
@@ -15,17 +18,31 @@ class DetailedAnalysis extends React.Component {
     return str.charAt(0).toUpperCase() + str.slice(1);
   }
 
+  filterValues(value) {
+    this.setState({entries_per_page: value, page: 1});
+  }
+
   render () {
-    console.dir(this.props.players);
-    const sorted = this.props.players.sort((a, b) => b[this.state.filter] - a[this.state.filter]);
-    const tableRows = sorted.map(player => { return (
-      <tr>
-        <td><img src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`} alt="<player_img>"></img></td>
-        <td>{player.firstName}</td>
+    const positions = ["GK", "DEF", "MID", "FWD"];
+    const positionPlayers = this.state.position !== "ALL" ? this.props.players.filter(player => player.position === (positions.indexOf(this.state.position) + 1)) : this.props.players;
+    const sorted = positionPlayers.sort((a, b) => b[this.state.filter] - a[this.state.filter]);
+    const page_entries = sorted.slice(0, this.state.entries_per_page);
+
+    const tableRows = page_entries.map(player => { return (
+      <tr className={"table-row"}>
+        <td><img className="thumbnail"src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player.code}.png`} alt=""></img></td>
         <td>{player.lastName}</td>
-        <td>{player.teamID}</td>
-        <td>{player.price}</td>
-        <td>{player[this.state.filter]}</td>
+        <td>{teamStrings[player.teamID - 1]}</td>
+        <td>{positions[player.position - 1]}</td>
+        <td>{player.form}</td>
+        <td>{player.totalPoints}</td>
+        <td>{player.price + "m"}</td>
+        <td>{player.goals}</td>
+        <td>{player.assists}</td>
+        <td>{player.clean_sheets}</td>
+        <td>{player.saves}</td>
+        <td>{player.pp_min}</td>
+        <td>{player.pp_mil}</td>
       </tr>
     )});
 
@@ -34,21 +51,40 @@ class DetailedAnalysis extends React.Component {
 
         <h1>Detailed Analysis</h1>
 
-        <table class="table table-hover table-dark">
-          <thead>
-            <tr>
-              <th scope="col">Picture</th>
-              <th scope="col">First</th>
-              <th scope="col">Last</th>
-              <th scope="col">Team</th>
-              <th scope="col">Price</th>
-              <th scope="col">{this.capitalizeFirst(this.state.filter)}</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableRows}
-          </tbody>
-        </table>
+        <div>
+
+        <TableFilter 
+          filterValues={(value) => this.filterValues(value)}
+          sortBy={(value) => this.setState({filter: value})}
+          position={(value) => this.setState({position: value})}
+        />
+
+          <table className="table table-hover table-dark">
+
+            <thead>
+              <tr className={"table-row"}>
+                <th scope="col"></th>
+                <th scope="col">Player</th>
+                <th scope="col">Team</th>
+                <th scope="col">POS</th>
+                <th scope="col">Form</th>
+                <th scope="col">Total Points</th>
+                <th scope="col">Price</th>
+                <th scope="col">Goals</th>
+                <th scope="col">Assists</th>
+                <th scope="col">Clean Sheets</th>
+                <th scope="col">Saves</th>
+                <th scope="col">Points per Minute</th>
+                <th scope="col">Points per Million</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {tableRows}
+            </tbody>
+
+          </table>
+        </div>
 
       </div>
     );
