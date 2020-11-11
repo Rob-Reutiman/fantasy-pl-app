1e8b746a0c5881a71b1f727c9ecb954b3c5dd176
Robert Reutiman <rreutima@nd.edu>
2020-11-11 18:30:04 -0500

    myTeam complete

--- src/myTeam.js
+++ src/myTeam.js
@@ -13,39 +13,56 @@ class MyTeam extends React.Component {
     }
   }
 
-  async handleSubmit(e, index) {
-    e.preventDefault();
+  async componentDidMount() {
+    setTimeout(async () => {
 
+    
+      await axios.post("http://127.0.0.1:8000/myTeam/", {
+        username: this.props.username,
+      })
+      .then((result) => {
+        console.dir(result.data)
+        this.setState(result.data["team"])
+      })
+
+    }, 1000);
+  }
+
+  async handleSubmit(e) {
+    e.preventDefault();    
+  }
+
+  async handleInput(e, index) {
     let temp = this.state;
-    let input = e.target.childNodes[0];
+    let input = e.target;
+    const value = e.target.value;
     if(input.name === "keepers") {
-      temp.GKs[index] = input.value;
+      temp.GKs[index] = value;
       this.setState({GKs: temp.GKs});
     } else if(input.name === "defenders") {
-      temp.DEFs[index] = input.value;
+      temp.DEFs[index] = value;
       this.setState({DEFs: temp.DEFs});
     } else if(input.name === "midfielders") {
-      temp.MIDs[index] = input.value;
+      temp.MIDs[index] = value;
       this.setState({MIDs: temp.MIDs});
     } else if(input.name === "forwards") {
-      temp.FWDs[index] = input.value;
+      temp.FWDs[index] = value;
       this.setState({FWDs: temp.FWDs});
     }
-  
-  // API save
-  await axios.post("http://127.0.0.1:8000/updateTeam/", {
-    username: this.props.username,
-    team: this.state
-  })
-  .then((result) => {
-    if(result.data["result"] !== "success") return;
-  })
 
-  // Set total price
-
+    // API save
+    await axios.post("http://127.0.0.1:8000/updateTeam/", {
+      username: this.props.username,
+      team: this.state
+    })
+    .then((result) => {
+      if(result.data["result"] !== "success") return;
+        })
   }
 
   render () {
+
+    console.dir(this.state)
 
     let totalPrice = 0;
 
@@ -62,53 +79,67 @@ class MyTeam extends React.Component {
       <option className="dropdown-item" value={player.lastName}>{player.lastName}</option>
     ));
 
-    const GKs = this.state.GKs.map((value, index) => (
+    const GKs = this.state.GKs.map((value, index) => {
+      const player = this.props.players.filter(player => player.position === 1).find(p => p.lastName == this.state.GKs[index]);
+      return (
       <div id={"GK" + index} className="player-entry" value={value}>
-        {this.state.GKs[index] != "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${this.props.players.filter(player => player.position === 1).find(p => p.lastName == this.state.GKs[index]).code}.png`} alt=""></img> : <img></img>}
-        <form onSubmit={e => this.handleSubmit(e, index)}>
-          <input name="keepers" list={"keepers" + index}/>
+        {this.state.GKs[index] != "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player !== undefined ? player.code : ""}.png`} alt=""></img> : <img></img>}
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={e => this.handleInput(e, index)} name="keepers" list={"keepers" + index} value={this.state.GKs[index]}/>
           <datalist id={"keepers" + index}>
             {keeperOptions}
           </datalist>
         </form>
       </div>
-    ));
+      )
+    });
 
-    const DEFs = this.state.DEFs.map((value, index) => (
-      <div id={"DEF" + index} className="player-entry" value={value}>
-        {this.state.DEFs[index] != "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${this.props.players.filter(player => player.position === 2).find(p => p.lastName == this.state.DEFs[index]).code}.png`} alt=""></img> : <img></img>}
-        <form onSubmit={e => this.handleSubmit(e, index)}>
-          <input name="defenders" list={"defenders" + index}/>
-          <datalist id={"defenders" + index}>
-            {defenderOptions}
-          </datalist>
-        </form>
-      </div>
-    ));
+    const DEFs = this.state.DEFs.map((value, index) => { 
 
-    const MIDs = this.state.MIDs.map((value, index) => (
+      const player = this.props.players.filter(player => player.position === 2).find(p => p.lastName == this.state.DEFs[index]);
+      return (
+        <div id={"DEF" + index} className="player-entry" value={value}>
+          {this.state.DEFs[index] != "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player !== undefined ? player.code : ""}.png`} alt=""></img> : <img></img>}
+          <form onSubmit={this.handleSubmit}>
+            <input onChange={e => this.handleInput(e, index)} name="defenders" list={"defenders" + index} value={this.state.DEFs[index]}/>
+            <datalist id={"defenders" + index}>
+              {defenderOptions}
+            </datalist>
+          </form>
+        </div>
+      )
+    });
+
+    const MIDs = this.state.MIDs.map((value, index) => { 
+
+      const player = this.props.players.filter(player => player.position === 3).find(p => p.lastName == this.state.MIDs[index]);
+      return (
       <div id={"MID" + index} className="player-entry" value={value}>
-        {this.state.MIDs[index] !== "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${this.props.players.filter(player => player.position === 3).find(p => p.lastName == this.state.MIDs[index]).code}.png`} alt=""></img> : <img></img>}
-        <form onSubmit={e => this.handleSubmit(e, index)}>
-          <input name="midfielders" list={"midfielders" + index}/>
+        {this.state.MIDs[index] !== "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player !== undefined ? player.code : ""}.png`} alt=""></img> : <img></img>}
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={e => this.handleInput(e, index)} name="midfielders" list={"midfielders" + index} value={this.state.MIDs[index]}/>
           <datalist id={"midfielders" + index}>
             {midfieldOptions}
           </datalist>
         </form>
       </div>
-    ));
+      )
+    });
 
-    const FWDs = this.state.FWDs.map((value, index) => (
+    const FWDs = this.state.FWDs.map((value, index) => {
+      const player = this.props.players.filter(player => player.position === 4).find(p => p.lastName == this.state.FWDs[index]);
+      return (
       <div id={"FWD" + index} className="player-entry" value={value}>
-        {this.state.FWDs[index] !== "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${this.props.players.filter(player => player.position === 4).find(p => p.lastName == this.state.FWDs[index]).code}.png`} alt=""></img> : <img></img>}
-        <form onSubmit={e => this.handleSubmit(e, index)}>
-          <input name="forwards" list={"forwards" + index}/>
+        {this.state.FWDs[index] !== "" ? <img className="thumbnail" src={`https://resources.premierleague.com/premierleague/photos/players/110x140/p${player !== undefined ? player.code : ""}.png`} alt=""></img> : <img></img>}
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={e => this.handleInput(e, index)} name="forwards" list={"forwards" + index} value={this.state.FWDs[index]}/>
           <datalist id={"forwards" + index}>
             {forwardOptions}
           </datalist>
         </form>
       </div>
-    ));
+      )
+    });
 
     return (
       <div className="section">
